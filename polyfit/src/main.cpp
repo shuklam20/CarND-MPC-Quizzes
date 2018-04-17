a04a1c510420dc68c4fe975b1aa3a7939b942316
Manas Shukla <shuklam@seas.upenn.edu>
2018-04-16 21:27:26 -0400

    Edited global_kinematic and polyfit

--- polyfit/src/main.cpp
+++ polyfit/src/main.cpp
@@ -49,9 +49,11 @@ int main() {
 
   // TODO: use `polyfit` to fit a third order polynomial to the (x, y)
   // coordinates.
+  Eigen::VectorXd poly_coeffs = polyfit(xvals, yvals, 3);
 
   for (double x = 0; x <= 20; x += 1.0) {
     // TODO: use `polyeval` to evaluate the x values.
+    std::cout << polyeval(poly_coeffs, x) << std::endl;
   }
 
   // Expected output
